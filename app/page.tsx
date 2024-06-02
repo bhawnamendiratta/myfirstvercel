9cdb2f467557501366f1741e07000dd3ac084631
bhawnamendiratta <bhawna9mendiratta@gmail.com>
2024-06-02 12:20:07 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { BlogPosts } from 'app/components/posts'
 
 export default function Page() {
   return (
-    <section>
+    
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         Interview
       </h1>
